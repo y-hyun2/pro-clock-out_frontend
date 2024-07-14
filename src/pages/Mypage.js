8f4322388920ae8710133c08ae9ea5cb98d3c738
Kang soyeong <korea020808@naver.com>
2024-07-14 14:30:12 +0900

    Feat: 대표 색상 상수 추가

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import theme from "../styles/theme";
+import styled from "styled-components";
+
+
+const ExampleDiv = styled.div`
+    background-color: ${theme.colors["main-sand"]};
+    color: ${theme.colors["main-purple"]};
+    width: 50px;
+    height: 50px;
+`;
 
 const Mypage = () => {
     return (
         <div>
             <h1>Login</h1>
+            <ExampleDiv>
+                HiHi
+            </ExampleDiv>
         </div>
     );
 };
