2957e31c05783ee8c3a269e67dcca9ac0aed79f5
luii0415 <becky45210193@gmail.com>
2024-07-24 22:01:43 +0900

    Feat: api 명세서에 맞추어 코드 수정

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { AiFillCaretDown } from "react-icons/ai";
 import Category from "../components/analytics/Category";
 import styled from "styled-components";
 import Analytics_BarChart from "../components/analytics/Analytics_BarChart";
@@ -25,21 +26,12 @@ const DropdownWrapper = styled.div`
   height: 30px;
   margin-top: 2%;
   margin-left: 2%;
-  padding: 5px 20px 10px 15px;
+  padding: 5px 15px 10px 15px;
   border: 1px solid #ccc;
-  border-radius: 25px;
+  border-radius: 15px;
   background-color: #7a7ee3;
   color: #fff;
   cursor: pointer;
-`;
-
-const WhiteCircle = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: #fff;
-  border-radius: 50%;
-  margin-top: 7px;
-  margin-right: 5px;
 `;
 
 const DropdownText = styled.div`
@@ -49,11 +41,15 @@ const DropdownText = styled.div`
   font-weight: bold;
   margin-top: 2px;
 `;
-
+const DropdownIcon = styled.div`
+  flex: 0.5;
+  margin-top: 8px;
+  margin-left: 10px;
+`;
 const DropdownOptions = styled.div`
   position: absolute;
   top: 100%;
-  margin-left: 2%;
+  margin-left: 1.8%;
   padding-top: 2px;
   padding-bottom: 2px;
   width: 120px;
@@ -87,12 +83,13 @@ const Title = styled.p`
 
 const TopCategoryContainer = styled.div`
   display: flex;
-  padding-bottom: 20px;
+  padding-bottom: 50px;
 `;
 const BottomCategoryContainer = styled.div`
   padding-left: 2%;
   display: flex;
   gap: 100px;
+  padding-bottom: 50px;
 `;
 const SynthesisScoreBox = styled.div``;
 const SynthesisScore = styled.div`
@@ -138,149 +135,206 @@ const LinegraphBox = styled.div`
   border: 0.5px gray solid;
 `;
 
-const Analytics = () => {
-  const [allChartData, setAllChartData] = useState([]);
+function Analytics() {
+  const [chartdata, setChartdata] = useState({
+    member_id: 1,
+    work_score: 82,
+    work_rank: 17,
+    work_avg: 71,
+    rest_score: 61,
+    rest_rank: 41,
+    rest_avg: 50,
+    sleep_score: 40,
+    sleep_rank: 63,
+    sleep_avg: 72,
+    personal_score: 96,
+    personal_rank: 3,
+    personal_avg: 71,
+    health_score: 28,
+    health_rank: 52,
+    health_avg: 84,
+  });
+
   const [period, setPeriod] = useState("주간");
   const [showOptions, setShowOptions] = useState(false);
-  const [synthesisdata, setSynthesisdata] = useState([
-    { name: "나의 점수", score: 82, fill: "#7A7EE3" },
-    { name: "평균 점수", score: 71, fill: "#DADBFF" },
-  ]);
-  const [horizondata, setHorizondata] = useState([
-    { name: "작업", score: 26.7, fill: "#8884d8" },
-    { name: "휴식", score: 19.9, fill: "#c6e0ff" },
-    { name: "수면", score: 13.0, fill: "#76e1e2" },
-    { name: "개인생활", score: 31.2, fill: "#97efb6" },
-    { name: "건강", score: 9.12, fill: "#7AA2E3" },
-  ]);
-  const [linedata, setLinedata] = useState([
-    {
-      date: "24.03.08",
-      value1: 65,
-      value2: 35,
-      value3: 45,
-      value4: 55,
-      value5: 60,
-    },
-    {
-      date: "24.03.23",
-      value1: 70,
-      value2: 38,
-      value3: 50,
-      value4: 58,
-      value5: 65,
-    },
-    {
-      date: "24.04.08",
-      value1: 59,
-      value2: 48,
-      value3: 50,
-      value4: 60,
-      value5: 55,
-    },
-    {
-      date: "24.04.23",
-      value1: 63,
-      value2: 45,
-      value3: 55,
-      value4: 62,
-      value5: 58,
-    },
-    {
-      date: "24.05.08",
-      value1: 80,
-      value2: 40,
-      value3: 55,
-      value4: 65,
-      value5: 70,
-    },
-    {
-      date: "24.05.23",
-      value1: 75,
-      value2: 50,
-      value3: 60,
-      value4: 68,
-      value5: 75,
-    },
-    {
-      date: "24.06.08",
-      value1: 81,
-      value2: 56,
-      value3: 60,
-      value4: 70,
-      value5: 78,
-    },
-    {
-      date: "24.06.23",
-      value1: 85,
-      value2: 60,
-      value3: 65,
-      value4: 72,
-      value5: 80,
-    },
-    {
-      date: "24.07.08",
-      value1: 56,
-      value2: 68,
-      value3: 65,
-      value4: 75,
-      value5: 85,
-    },
-  ]);
+
   const categories = [
     {
       title: "작업",
-      score: 82,
-      percentage: 17,
-      fill: allChartData[0]?.fill,
-      data: allChartData.slice(0, 2),
+      score: chartdata.work_score,
+      percentage: chartdata.work_rank,
+      fill: "#7AA2E3",
+      data: [
+        { name: "나의 점수", score: chartdata.work_score, fill: "#7AA2E3" },
+        { name: "평균 점수", score: chartdata.work_avg, fill: "#C9DDFD" },
+      ],
     },
+
     {
       title: "휴식",
-      score: 61,
-      percentage: 41,
-      fill: allChartData[2]?.fill,
-      data: allChartData.slice(2, 4),
+      score: chartdata.rest_score,
+      percentage: chartdata.rest_rank,
+      fill: "#A2A6FF",
+      data: [
+        { name: "나의 점수", score: chartdata.rest_score, fill: "#A2A6FF" },
+        { name: "평균 점수", score: chartdata.rest_avg, fill: "#EEEFFF" },
+      ],
     },
     {
       title: "수면",
-      score: 40,
-      percentage: 63,
-      fill: allChartData[4]?.fill,
-      data: allChartData.slice(4, 6),
+      score: chartdata.sleep_score,
+      percentage: chartdata.sleep_rank,
+      fill: "#6AD4DD",
+      data: [
+        { name: "나의 점수", score: chartdata.sleep_score, fill: "#6AD4DD" },
+        { name: "평균 점수", score: chartdata.sleep_avg, fill: "#B4F9FF" },
+      ],
     },
     {
       title: "개인 생활",
-      score: 96,
-      percentage: 3,
-      fill: allChartData[6]?.fill,
-      data: allChartData.slice(6, 8),
+      score: chartdata.personal_score,
+      percentage: chartdata.personal_rank,
+      fill: "#97E7E1",
+      data: [
+        { name: "나의 점수", score: chartdata.personal_score, fill: "#97E7E1" },
+        { name: "평균 점수", score: chartdata.personal_avg, fill: "#CCFFFB" },
+      ],
     },
     {
       title: "건강",
-      score: 28,
-      percentage: 52,
-      fill: allChartData[8]?.fill,
-      data: allChartData.slice(8, 10),
+      score: chartdata.health_score,
+      percentage: chartdata.health_rank,
+      fill: "#F8F6E3",
+      data: [
+        { name: "나의 점수", score: chartdata.health_score, fill: "#F8F6E3" },
+        { name: "평균 점수", score: chartdata.health_avg, fill: "#FFFEF0" },
+      ],
     },
   ];
-  const [showLineGraph, setShowLineGraph] = useState(false);
+  const total_my_score =
+    chartdata.work_score +
+    chartdata.rest_score +
+    chartdata.sleep_score +
+    chartdata.personal_score +
+    chartdata.health_score;
 
-  useEffect(() => {
-    const data = [
-      { name: "나의 점수", score: 82, fill: "#7AA2E3" },
-      { name: "평균 점수", score: 71, fill: "#C9DDFD" },
-      { name: "나의 점수", score: 61, fill: "#A2A6FF" },
-      { name: "평균 점수", score: 50, fill: "#EEEFFF" },
-      { name: "나의 점수", score: 40, fill: "#6AD4DD" },
-      { name: "평균 점수", score: 72, fill: "#B4F9FF" },
-      { name: "나의 점수", score: 96, fill: "#97E7E1" },
-      { name: "평균 점수", score: 71, fill: "#CCFFFB" },
-      { name: "나의 점수", score: 28, fill: "#F8F6E3" },
-      { name: "평균 점수", score: 84, fill: "#FFFEF0" },
-    ];
-    setAllChartData(data);
-  }, []);
+  const [synthesisdata, setSynthesisdata] = useState([
+    {
+      name: "나의 점수",
+      score: Math.round(total_my_score / 5),
+      fill: "#7A7EE3",
+    },
+    { name: "평균 점수", score: 71, fill: "#DADBFF" },
+  ]);
+
+  const horizondata = [
+    {
+      name: "작업",
+      score: Number(((chartdata.work_score / total_my_score) * 100).toFixed(1)),
+      fill: "#8884d8",
+    },
+    {
+      name: "휴식",
+      score: Number(((chartdata.rest_score / total_my_score) * 100).toFixed(1)),
+      fill: "#c6e0ff",
+    },
+    {
+      name: "수면",
+      score: Number(
+        ((chartdata.sleep_score / total_my_score) * 100).toFixed(1)
+      ),
+      fill: "#76e1e2",
+    },
+    {
+      name: "개인생활",
+      score: Number(
+        ((chartdata.personal_score / total_my_score) * 100).toFixed(1)
+      ),
+      fill: "#97efb6",
+    },
+    {
+      name: "건강",
+      score: Number(
+        ((chartdata.health_score / total_my_score) * 100).toFixed(1)
+      ),
+      fill: "#FFFBD4",
+    },
+  ];
+  const [linedata, setLinedata] = useState({
+    total: [
+      { "2024-07-08": 8 },
+      { "2024-07-01": 7 },
+      { "2024-06-24": 6 },
+      { "2024-06-17": 5 },
+      { "2024-06-10": 6 },
+      { "2024-06-03": 7 },
+      { "2024-05-27": 8 },
+      { "2024-05-20": 2 },
+      { "2024-05-13": 3 },
+      { "2024-05-06": 5 },
+    ],
+    work: [
+      { "2024-07-08": 88 },
+      { "2024-07-01": 68 },
+      { "2024-06-24": 65 },
+      { "2024-06-17": 25 },
+      { "2024-06-10": 46 },
+      { "2024-06-03": 72 },
+      { "2024-05-27": 83 },
+      { "2024-05-20": 92 },
+      { "2024-05-13": 93 },
+      { "2024-05-06": 70 },
+    ],
+    rest: [
+      { "2024-07-08": 20 },
+      { "2024-07-01": 72 },
+      { "2024-06-24": 21 },
+      { "2024-06-17": 55 },
+      { "2024-06-10": 56 },
+      { "2024-06-03": 72 },
+      { "2024-05-27": 85 },
+      { "2024-05-20": 52 },
+      { "2024-05-13": 53 },
+      { "2024-05-06": 55 },
+    ],
+    sleep: [
+      { "2024-07-08": 40 },
+      { "2024-07-01": 74 },
+      { "2024-06-24": 64 },
+      { "2024-06-17": 45 },
+      { "2024-06-10": 66 },
+      { "2024-06-03": 44 },
+      { "2024-05-27": 84 },
+      { "2024-05-20": 42 },
+      { "2024-05-13": 93 },
+      { "2024-05-06": 25 },
+    ],
+    personal: [
+      { "2024-07-08": 82 },
+      { "2024-07-01": 28 },
+      { "2024-06-24": 61 },
+      { "2024-06-17": 75 },
+      { "2024-06-10": 62 },
+      { "2024-06-03": 72 },
+      { "2024-05-27": 87 },
+      { "2024-05-20": 72 },
+      { "2024-05-13": 73 },
+      { "2024-05-06": 72 },
+    ],
+    health: [
+      { "2024-07-08": 20 },
+      { "2024-07-01": 55 },
+      { "2024-06-24": 41 },
+      { "2024-06-17": 35 },
+      { "2024-06-10": 46 },
+      { "2024-06-03": 32 },
+      { "2024-05-27": 51 },
+      { "2024-05-20": 32 },
+      { "2024-05-13": 33 },
+      { "2024-05-06": 55 },
+    ],
+  });
+  const [showLineGraph, setShowLineGraph] = useState(false);
 
   const toggleOptions = () => {
     setShowOptions(!showOptions);
@@ -299,8 +353,10 @@ const Analytics = () => {
     <Container>
       <DropdownContainer>
         <DropdownWrapper onClick={toggleOptions}>
-          <WhiteCircle />
           <DropdownText>{period}</DropdownText>
+          <DropdownIcon>
+            <AiFillCaretDown size={20} color="#FFFBD4" />
+          </DropdownIcon>
         </DropdownWrapper>
         {showOptions && (
           <DropdownOptions>
@@ -327,7 +383,7 @@ const Analytics = () => {
         <SynthesisScoreBox>
           <Title>종합 워라벨 점수</Title>
           <SynthesisScore>
-            <Score>76점</Score>
+            <Score>{Math.round(total_my_score / 5)}점</Score>
             <Percentage>상위 22%</Percentage>
             <Analytics_BarChart data={synthesisdata} />
           </SynthesisScore>
@@ -348,6 +404,6 @@ const Analytics = () => {
       </BottomCategoryContainer>
     </Container>
   );
-};
+}
 
 export default Analytics;
