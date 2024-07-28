bdd59d38d4f30e3d193b126ef1537adaeba3b077
Kang soyeong <korea020808@naver.com>
2024-07-29 01:46:11 +0900

    추구하는 삶 기능 추가

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,14 +1,24 @@
+// Mypage.jsx
+
 import React, { useState } from "react";
 import styled from "styled-components";
 import defaultImage from "../img/mypage-default.png";
 import cameraLogo from "../img/cameraLogo.png";
 import theme from "../styles/theme";
 import { useNavigate } from "react-router-dom";
+import Lifelist from "../components/mypage/Lifelist";
 
 const Mypage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  // 이미지 업로드 state
   const [uploadedImage, setUploadedImage] = useState(null);
+
+  // Lifelist 컴포넌트 state
+  const [isLifelistVisible, setIsLifelistVisible] = useState(false);
+
+  // Store selected lifestyles
+  const [selectedLifestyles, setSelectedLifestyles] = useState([]);
 
   const onChangeImage = (e) => {
     const file = e.target.files[0];
@@ -28,7 +38,7 @@ const Mypage = () => {
   let life = "균형 잡힌";
 
   //서버에서 닉네임 받아와야 함
-  let nickname = "헥스코드"
+  let nickname = "헥스코드";
 
   //서버에서 점수 받아와야함
   let score = 88;
@@ -37,7 +47,19 @@ const Mypage = () => {
   let percentage = 11;
 
   //추후 AI 개선사항 동적으로 받아와야 함
-  let aiSolutionContent = "밥도 잘 먹고 공부도 열심히 하고 잠을 잘 자세요. 어디까지나 AI 솔루션이므로 자세한 진단은 병원에 가서 받아보세요. 어제 저녁에는 매운 음식을 드셨군요. 매운 음식은 소화기관에 좋지 않으니 자제하기 바랍니다. 회사에서 너무 심한 압박감을 느낀다면 휴가를 내고 쉬는 것을 권장합니다.";
+  let aiSolutionContent =
+    "밥도 잘 먹고 공부도 열심히 하고 잠을 잘 자세요. 어디까지나 AI 솔루션이므로 자세한 진단은 병원에 가서 받아보세요. 어제 저녁에는 매운 음식을 드셨군요. 매운 음식은 소화기관에 좋지 않으니 자제하기 바랍니다. 회사에서 너무 심한 압박감을 느낀다면 휴가를 내고 쉬는 것을 권장합니다.";
+
+  // Lifelist 버튼 클릭 시 상태 변경 (클릭시 렌더링)
+  const toggleLifelistVisibility = () => {
+    setIsLifelistVisible(!isLifelistVisible);
+  };
+
+  // Lifelist 관리
+  const handleLifelistSelect = (selectedLifestyles) => {
+    setSelectedLifestyles(selectedLifestyles);
+    setIsLifelistVisible(false);
+  };
 
   return (
     <div>
@@ -60,11 +82,11 @@ const Mypage = () => {
 
           <TextArea>
             <TextWrapper>
-              <Text>닉네임    |   </Text>
+              <Text>닉네임 | </Text>
               <br></br>
-              <Text>이메일    |   </Text>
+              <Text>이메일 | </Text>
               <br></br>
-              <Text>추구하는 삶   |   </Text>
+              <Text>추구하는 삶 | </Text>
               <br></br>
             </TextWrapper>
             <InfoWrapper>
@@ -72,31 +94,48 @@ const Mypage = () => {
               <br></br>
               <Text> example@email.kr</Text>
               <br></br>
-              <LifeStyle>건강하고 행복한 삶</LifeStyle>
-              <br></br>
+
+              <LifestyleTags>
+                {selectedLifestyles.length > 0 ? (
+                  selectedLifestyles.map((lifestyle) => (
+                    <LifestyleTag key={lifestyle.id}>
+                      {lifestyle.data}
+                    </LifestyleTag>
+                  ))
+                ) : (
+                  <NoSelection>추구하는 삶을 선택해주세요</NoSelection>
+                )}
+                <LifelistButton onClick={toggleLifelistVisibility}>
+                  +
+                </LifelistButton>
+              </LifestyleTags>
             </InfoWrapper>
           </TextArea>
         </LeftWrapper>
 
         <Divider></Divider>
         <RightWrapper>
-
-            <Cheer>시작이 반이다. 화이팅!</Cheer>
-            <RightText>나의 워라벨</RightText>
-            <Balance>
-                <BalanceTitle>{life} 삶을 사는 <Nickname>{nickname}</Nickname>님</BalanceTitle>
-                <Score>{score}점</Score>
-                <Percentage>상위 {percentage}%</Percentage>
-                <DetailView onClick={goToAnalytics}>상세보기</DetailView>
-            </Balance>
-            <RightText>AI 개선 방향 제안</RightText>
-            <AiSolution>
-                <AiSolutionContent>{aiSolutionContent}</AiSolutionContent>
-            </AiSolution>
-            <Logout onClick={goToLogin}>로그아웃</Logout>
-            <Withdrawal>회원 탈퇴</Withdrawal>
+          <Cheer>시작이 반이다. 화이팅!</Cheer>
+          <RightText>나의 워라벨</RightText>
+          <Balance>
+            <BalanceTitle>
+              {life} 삶을 사는 <Nickname>{nickname}</Nickname>님
+            </BalanceTitle>
+            <Score>{score}점</Score>
+            <Percentage>상위 {percentage}%</Percentage>
+            <DetailView onClick={goToAnalytics}>상세보기</DetailView>
+          </Balance>
+          <RightText>AI 개선 방향 제안</RightText>
+          <AiSolution>
+            <AiSolutionContent>{aiSolutionContent}</AiSolutionContent>
+          </AiSolution>
+          <Logout onClick={goToLogin}>로그아웃</Logout>
+          <Withdrawal>회원 탈퇴</Withdrawal>
         </RightWrapper>
       </Wrapper>
+
+      {/* Lifelist 컴포넌트 조건부 렌더링 */}
+      {isLifelistVisible && <Lifelist onSelect={handleLifelistSelect} />}
     </div>
   );
 };
@@ -112,8 +151,6 @@ const Wrapper = styled.div`
   margin-top: 10rem;
 `;
 
-// const SideWrapper
-
 //왼쪽 (Divider 미포함) 감싸는 Wrapper
 const LeftWrapper = styled.div`
   display: flex;
@@ -123,13 +160,13 @@ const LeftWrapper = styled.div`
 const Divider = styled.div`
   width: 0.3rem;
   height: 70rem;
-  margin-left: 10rem;
+  margin-left: 0rem;
   background-color: lightgray;
 `;
 
 //프로필 사진, "프로필 사진 변경하기", 카메라 로고 Wrapper
 const ImageWrapper = styled.div`
-  margin-left: 8rem;
+  margin-left: 7rem;
   display: flex;
   flex-direction: column;
 `;
@@ -179,6 +216,7 @@ const CameraIcon = styled.img`
 const TextArea = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
 `;
 //닉네임, 이메일, 추구하는 삶 Wrapper
 const TextWrapper = styled.div`
@@ -187,146 +225,170 @@ const TextWrapper = styled.div`
 `;
 
 const Text = styled.p`
-  margin-top: 1rem;
-  margin-bottom: 0;
+  margin-top: 0.7rem;
+  margin-bottom: -0.2rem;
   font-size: 2.4rem;
   font-weight: bold;
   white-space: pre;
-`;
-
-//추구하는 삶 선택 버튼. 추후 색 변경 및 동적 코드로 변경 필요.
-const LifeStyle = styled.button`
-  width: 36rem;
-  height: 6rem;
-  margin-left: 0.5rem;
-  font-size: 2.4rem;
-  font-weight: bold;
-  border-radius: 1.3rem;
-  border: none;
-  color: white;
-  background-color: ${theme.colors["main-blue"]};
 `;
 
 //헥스코드, example, 추구하는 삶 버튼 Wrapper
 const InfoWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
+  width: 48rem;
+`;
+
+// 추구하는 삶 태그들
+const LifestyleTags = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: 0.5rem;
+`;
+
+//추구하는 삶 선택 버튼.
+const LifelistButton = styled.button`
+  font-size: 1.8rem;
+  font-weight: 100;
+  border-radius: 1rem;
+  border: none;
+  color: #afafaf;
+  background: transparent;
+  cursor: pointer;
+  margin-left: 0.5rem;
+  margin-top: -0.1rem;
 `;
 
 //여기까지 왼쪽
 
 //여기부터 오른쪽
 const RightWrapper = styled.div`
-    margin-left: 7rem;
-    margin-top: -4.5rem;
+  margin-left: 7rem;
+  margin-top: -4.5rem;
 `;
 
 //AI 활용한 응원 문구. 우선 Static하게 넣어둠.
 const Cheer = styled.h1`
-    font-size: 4.5rem;
-    margin-top: 0;
-    margin-bottom: 5rem;
-    text-align: center;
+  font-size: 4.5rem;
+  margin-top: 0;
+  margin-bottom: 5rem;
+  text-align: center;
 `;
 
 //"나의 워라벨", "AI 개선 방향 제안" 텍스트 조정 코드
 const RightText = styled.h2`
-    font-size: 2.5rem;
-    margin-bottom: 1.2rem;
-    margin-left: 0.1rem;
-    margin-top: 0rem;
+  font-size: 2.5rem;
+  margin-bottom: 1.2rem;
+  margin-left: 0.1rem;
+  margin-top: 0rem;
 `;
-
-
 
 //워라벨 div
 const Balance = styled.div`
-align-items: center;
-text-align: center;
-    width: 68rem;
-    height: 30rem;
-    border: 0.3rem solid;
-    border-radius: 2rem;
-    border-color: lightgray;
-    margin-bottom: 4.5rem;
+  align-items: center;
+  text-align: center;
+  width: 68rem;
+  height: 30rem;
+  border: 0.3rem solid;
+  border-radius: 2rem;
+  border-color: lightgray;
+  margin-bottom: 4.5rem;
 `;
 
 //OO한 삶을 사는 {닉네임} 님
 const BalanceTitle = styled.p`
-margin-bottom: 0;
-margin-top: 4rem;
-    font-size: 3rem;
-    font-weight: 500;
-
+  margin-bottom: 0;
+  margin-top: 4rem;
+  font-size: 3rem;
+  font-weight: 500;
 `;
 
 //닉네임
 const Nickname = styled.span`
   font-weight: bold;
-  color: ${theme.colors["main-purple"]}; 
+  color: ${theme.colors["main-purple"]};
 `;
 
 //점수
 const Score = styled.h1`
-    font-size: 6rem;
-    margin-bottom: 0;
-    margin-top: 0rem;
+  font-size: 6rem;
+  margin-bottom: 0;
+  margin-top: 0rem;
 `;
 
 //상위 n%
 const Percentage = styled.p`
-margin-top: 0rem;
-margin-bottom: 1.3rem;
-font-size: 2rem;
+  margin-top: 0rem;
+  margin-bottom: 1.3rem;
+  font-size: 2rem;
 `;
 
 const DetailView = styled.button`
-    width: 13rem;
-    height: 4rem;
-    border: none;
-    font-size: 1.7rem;
-    font-weight: bold;
-    border-radius: 0.8rem;
-    color: white;
-    background-color: ${theme.colors["main-purple"]};;
+  width: 13rem;
+  height: 4rem;
+  border: none;
+  font-size: 1.7rem;
+  font-weight: bold;
+  border-radius: 0.8rem;
+  color: white;
+  background-color: ${theme.colors["main-purple"]};
+  cursor: pointer;
 `;
 
 const AiSolution = styled.div`
-    width: 68rem;
-    height: 15rem;
-    border: 0.3rem solid;
-    border-radius: 2rem;
-    border-color: lightgray;
-    
+  width: 68rem;
+  height: 15rem;
+  border: 0.3rem solid;
+  border-radius: 2rem;
+  border-color: lightgray;
 `;
 
 const AiSolutionContent = styled.p`
- font-size: 2.2rem;
- margin-top: 0.9rem;
- margin-left: 0.7rem;
+  font-size: 2.2rem;
+  margin-top: 0.9rem;
+  margin-left: 0.7rem;
 `;
 
 const Logout = styled.button`
-    width: 12rem;
-    height: 5rem;
-    background-color: lightgrey;
-    border: none;
-    border-radius: 1rem;
-    margin-top: 4rem;
-    margin-left: 45rem;
-    font-size: 2.3rem;
-    font-weight: bold;
-    color: white;
+  width: 12rem;
+  height: 5rem;
+  background-color: lightgrey;
+  border: none;
+  border-radius: 1rem;
+  margin-top: 4rem;
+  margin-left: 45rem;
+  font-size: 2.3rem;
+  font-weight: bold;
+  color: white;
 `;
 
 //탈퇴
 const Withdrawal = styled.button`
-background: transparent;
-border:none;
-color: lightgray;
-font-size: 2.3rem;
-font-weight: bold;
-margin-left: 1rem;
+  background: transparent;
+  border: none;
+  color: lightgray;
+  font-size: 2.3rem;
+  font-weight: bold;
+  margin-left: 1rem;
 `;
 
+const LifestyleTag = styled.div`
+  background-color: ${theme.colors["main-light-green"]};
+  color: #373a80;
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  margin-right: 0.5rem;
+  margin-top: 0.5rem;
+`;
 
+const NoSelection = styled.p`
+  font-size: 1.5rem;
+  color: grey;
+  margin-top: 1rem;
+  margin-left: 0.6rem;
+  display: inline;
+`;
